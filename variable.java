1dc881bba6327669c5482343e920f01865b23e71
KimYeongChan <goldfish2679@naver.com>
2022-10-13 22:01:42 +0900

    variable

---
+++ variable.java
@@ -0,0 +1,141 @@
+// chapter2 - variable
+import java.util.*;
+class Bye {
+	public static void main (String[] args) {
+		System.out.println("Hello, world."); //화면에 글자를 출력한다
+
+	}
+}
+class VarEx1 {
+	public static void main(String[] args) {
+		int year=0;//자료형(data type / int) 선언 후 초기화, 초기화(initialization)란 변수를 선언하고 메모리에 남아있는 혹시 모를 쓰레기 값을 지우기 위해 값을 대입하는 것.
+		int age=14;
+
+		System.out.println(year);//=print()
+		System.out.println(age);
+		year+=2000;
+		age+=1;
+		System.out.println(year+age);
+		System.out.println(age);
+		int b=3;
+		System.out.println(b);
+	}
+}
+
+class Constant {
+	public static void main(String[] args) {
+		final int CONS=30; //상수(constant)는 선언과 동시에 초기화해야만 함, 상수의 이름은 모두 대문자를 '권장'.
+		System.out.println(CONS);
+	}
+}
+
+class String1 { //class 선언시 예약어 사용 불가능(String)
+	public static void main(String[] args) {
+		String str="java";
+		System.out.println(str+8.0);//문자열과 숫자의 연산이 가능한 것을 보여줌. 문자형(char)은 작은 따옴표를 쓰며 문자1개만 저장 가능.
+	}
+}
+
+class specifier {
+	public static void main(String[] args) {
+		int age = 14;
+		System.out.printf("%d살 입니다%n",age);//printf는 줄바꿈x. \n도 가능하지만 os별 줄바꿈 기능이 다르니 %n이 가장 이상적.
+		System.out.printf("%d살 입니다%n",14);//파이썬의 문자열 포매팅 개념. 
+		int year = 2000;
+		System.out.printf("%d년에는 %d살입니다",year,age);
+		byte b=1;
+		short s=2;
+		char c='A';
+		int finger=10;
+		int octNum=010;
+		int hexNum=0x10;
+		int binNum=0b10;
+		System.out.printf("b:%d%n",b);
+		System.out.printf("%5d%n",finger); //파이썬에도 있는 문자열 공백 처리에 관한 기능
+		System.out.printf("%-5d%n",finger);
+		System.out.printf("%010d%n",finger);
+		System.out.printf("%o, %d %n",octNum, octNum);
+		System.out.printf("%x, %d %n",hexNum, hexNum);
+		System.out.printf("binNum=%s%n", Integer.toBinaryString(binNum));
+		System.out.printf("%c %d",c,(int)c);
+		
+
+	}
+}
+
+
+class scanner {
+	public static void main(String []args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("정수를 입력하세요");
+		String input = scanner.nextLine();
+		System.out.println("입력내용(정수변환전) :"+input);
+		int num = Integer.parseInt(input);
+
+		System.out.println("입력내용(정수변환후) :"+input);
+		System.out.printf("입력내용(printf+ 지시자를 이용) : %d",num);
+		
+	}
+}
+
+class CharToCode {
+	public static void main(String [] args) {
+		char ch = 'A' ;// char ch=65;와 동일, 문자형에는 유니코드값으로도 문자를 저장 가능함.
+		char ch2 = 65;// 컴퓨터에 저장되는 값은 'A'든 65든 이진수로 동일한 값이 저장되지만 출력은 다르게 나온다. 그 이유는 println은 저장된 값을
+		int num = (int)ch; // 리터럴?변수의 타입에 맞게 출력하기 때문. int면 2진수를 10진수로, char면 2진수를  유니코드로 출력.
+
+		System.out.printf("문자로 저장 : %c , 유니코드로 저장 : %c %n", ch,ch2);
+		System.out.printf("정수로 변환 : %d %n", num);
+		System.out.printf("%x, %#x %n", num,num);
+
+	}
+}
+
+class Overflow {
+	public static void main(String [] args) {
+		short sMin=-32768;// short의 데이터 저장공간은 2byte로 부호있는 숫자의 경우 -32768~32767이다.
+		// short test=-32769; 다음과 같이 -32768보다 작은 숫자로 변수선언후 초기화시 에러발생. 저장공간을 초과한다는 뜻의 에러메시지.
+		short sMax=32767;
+		char cMin=0;
+		char cMax=65535;
+		
+		System.out.println("sMin : " + sMin);
+		System.out.println("sMin-1 : " + (short)(sMin-1));//위의 test 변수와는 다르게 다음과 같이 다시 변수선언을 하게 되면 원하는 결과값이 나온다.
+		//만약 short가 없다면 sMin-1에 해당하는  -32768이 출력. 왜냐하면 그 숫자를 short 정수형에 저장한 것이 아니기 때문.
+		System.out.println("cMax+1 : "+(int)(++cMax));//++의 기능을 잘 모르겠다 (int)(cMax+1)하면 65536출력됨.
+	}
+}
+
+class FloatEx {
+	public static void main(String [] args) {
+		float f1 = 9.123456789012345678f;
+		float f2 = 1.123456789012345678f;
+		double d1 = 3.12345678901234567890;//d 생략가능 기본자료형이라(default data type)
+		System.out.printf("f1 출력 : %f %n", f1);
+		System.out.printf("f1 자리수 지정 후 출력 : %.10f %n",f1);//%전체자리.소수점아래자리f , 정수부분의 빈자리는 공백, 소수부분은 0으로 채운다.
+		System.out.printf("f2 출력 : %f %n",f2);
+		System.out.printf("f2 자리수 지정 후 출력 : %-25.10f 공백확인%n",f2);
+		System.out.printf("d1 출력 : %f %n",d1);//%f는 기본적으로 6자리까지 출력. 실수형의 타입과 관계없음(double or float)   
+		System.out.printf("d1 자리수 지정 후 출력 : %25.20f",d1);
+	}
+}
+
+class CastingEx {
+	public static void main(String [] args) {
+		int i=126, j=129;
+		byte b=(byte)i;
+		byte c=(byte)j;//byte형은 1byte의 데이터만 저장할 수 있다. 즉 2의8승 범위로 보면 -128~127사이의 숫자만 저장 가능
+		System.out.printf(" i : %d j : %d %n", b,c);
+
+		int width = 20;
+		int height = 10;
+		int triangle = (width*height)/2;
+		int square = width*height;// 왜 상수를 써야하는지 잘 모르겠다. 어차피 변수 새로 지정하나 상수쓰나 똑같은데
+		System.out.printf("triangle : %d , square : %d %n",triangle, square);
+		float pi = 3;
+		long num = 2*10000000000l;
+		System.out.printf("실수 : %f 정수long : %d ",pi,num);
+		 
+	}
+}
